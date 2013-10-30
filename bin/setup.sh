56c03be045ff63576b432b60bdf2ba9d6f7a5aa0
Mitsutoshi Aoe <maoe@foldr.in>
2013-10-30 10:48:44 +0900

    bin/setup.sh: Better error handling

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
+DOTFILES_DIR=$HOME/dotfiles
 
 ssh $@ <<EOF
-git clone --recursive https://github.com/maoe/dotfiles.git
-cd dotfiles
-rake install
+type git || echo "You need git to proceed"
+type rake || echo "You need rake to proceed"
+
+if [ -d "$DOTFILES_DIR" ]; then
+  cd $DOTFILES_DIR
+  git pull
+else
+  git clone --recursive https://github.com/maoe/dotfiles.git $DOTFILES_DIR
+fi
+cd $DOTFILES_DIR
+yes o | rake install
 EOF
