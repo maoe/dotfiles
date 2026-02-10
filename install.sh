9a9ae866b00e1b777a73d4ffdb93d803f3c80de4
Mitsutoshi Aoe <me@maoe.name>
2026-02-10 09:48:40 +0900

    Add starship config as a stow package
    Manage ~/.config/starship.toml via stow so the prompt configuration
    is consistent across machines.

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-PACKAGES="git haskell rc ruby tig tmux vim zsh"
+PACKAGES="git haskell rc ruby starship tig tmux vim zsh"
 
 for pkg in $PACKAGES; do
     stow -v --target="$HOME" "$pkg"
